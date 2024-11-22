2b0806235ae7812aeef54c50a9f6160e32173c60
Nassim Mengat <nassim.mengat@hva.nl>
2024-11-22 17:50:33 +0000

    Nov 22, 2024, 6:50 PM

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is my Flask app in Docker3!'wivuhwvw333
+    return 'Hello, World! This is my Flask app in Docker3444!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
