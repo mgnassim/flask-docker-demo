195595f568e10f0dfd9ef61461bb5e386c072c0d
Nassim Mengat <nassim.mengat@hva.nl>
2024-11-22 18:37:48 +0100

    Simulating a failure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is my Flask app in Docker3!'
+    return 'Hello, World! This is my Flask app in Docker3!'wivuhwvw
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
