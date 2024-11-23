925d243ffb36f136fb176a062996d603a6cbd719
Nassim Mengat <nassim.mengat@hva.nl>
2024-11-23 19:21:10 +0100

    testing deploy to google cloud

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello, World! This is my Flask app in Docker (google cloud v1)!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
